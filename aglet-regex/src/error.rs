737e272ce3c544dd32a283cf0887b1fcfe1783b3
Carson Myers <carson@myers.se>
2021-09-03 21:41:48 +0200

    start regex lib

---
+++ aglet-regex/src/error.rs
@@ -0,0 +1,9 @@
+use color_eyre::{
+    eyre::{eyre, Report}
+};
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum TokenizeError {
+
+}
--- aglet-regex/src/lib.rs
+++ aglet-regex/src/lib.rs
@@ -1,3 +1,72 @@
+pub enum Token {
+    Literal(char),
+    Or,
+
+    // Character classes
+    OpenBracket,
+    CloseBracket,
+    Negated,
+    Range,
+    RangeSubtract,
+    RangeDifference,
+    RangeIntersection,
+    Any,
+    Digit,
+    NonDigit,
+    AlphaNum,
+    NonAlphaNum,
+    Whitespace,
+    NonWhitespace,
+    Bell,
+    Tab,
+    Return,
+    Linefeed,
+    VTab,
+    FormFeed,
+    Backspace,
+    Null,
+    NamedClass(String),
+    NonNamedClass(String),
+    Control(char),
+    CharCode(i8),
+    UTF16(i16),
+    Codepoint(i32),
+    UnicodeProperty(String),
+    NonUnicodeProperty(String),
+
+    // Boundaries
+    InputStart,
+    MultilineInputStart,
+    InputEnd,
+    MultilineInputEnd,
+    WordBoundary,
+    NonWordBoundary,
+
+    // Quantifiers
+    OpenBrace,
+    CloseBrace,
+    Number(i32),
+    Comma,
+    ZeroOrMore,
+    OneOrMore,
+    ZeroOrOne,
+    Lazy,
+
+    // Groups
+    OpenGroup,
+    CloseGroup,
+    NonCapturing,
+    Name(String),
+    BackReference(i32),
+    NamedBackReference(String),
+    Flags(Vec<char>),
+    NonCapturingFlags(Vec<char>),
+}
+
+pub fn tokenize<S>(input: &str) -> Result<Vec<Token>> {
+
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
